65fc23187090408ff0c336b9a11b2abe7a282f3a
kenhanbit <147574395+kenhanbit@users.noreply.github.com>
2024-10-14 23:32:36 +0700

    page 1 ( temporary )
    kenkensusu

--- app/src/main/java/com/example/project_kelompok3/MainActivity.kt
+++ app/src/main/java/com/example/project_kelompok3/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+            insets // Pastikan insets dikembalikan untuk menyelesaikan error
         }
     }
 }
